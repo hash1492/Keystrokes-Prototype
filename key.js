798519de98fbf6fb3246a42f05ea853b36a99328
Harshil Shah <hash_1492@yahoo.com>
2014-09-10 16:21:37 +0530

    Fixes #10 and adds opening bracket in same line as if statement

--- key.js
+++ key.js
@@ -7,8 +7,7 @@ $(document).keydown(function (e) {
 		var abc=String.fromCharCode(e.keyCode);
 
 		//Single key press
-		if(e.keyCode)
-		{
+		if(e.keyCode) {
 			console.log(abc);
 			$("#pid1").text(result.key[abc]);
 		
@@ -32,215 +31,173 @@ $(document).keydown(function (e) {
 			$("#pid1").text("shift + " +result.key[abc]);
 		}
 
-		if(e.keyCode==32)
-		{
+		if(e.keyCode==32) {
 			$("#pid1").text("Space");
 		}
 
 
-		if(e.keyCode==27)
-		{
+		if(e.keyCode==27) {
 			$("#pid1").text("Escape");
 		}
 
 
-		if(e.keyCode==8)
-		{
+		if(e.keyCode==8) {
 			$("#pid1").text("Backspace");
 		}
 
-		if(e.keyCode==9)
-		{
+		if(e.keyCode==9) {
 			$("#pid1").text("Tab");
 		}
 
-		if(e.keyCode==13)
-		{
+		if(e.keyCode==13) {
 			$("#pid1").text("Enter");
 		}
 		
-		if(e.keyCode==18)
-		{
+		if(e.keyCode==18) {
 			$("#pid1").text("Alt");
 		}
 
-		if(e.keyCode==20)
-		{
+		if(e.keyCode==20) {
 			$("#pid1").text("Caps Lock");
 		}
 		
-		if(e.keyCode==33)
-		{
+		if(e.keyCode==33) {
 			$("#pid1").text("Page Down");
 		}
 		
-		if(e.keyCode==34)
-		{
+		if(e.keyCode==34) {
 			$("#pid1").text("Page Up");
 		}
 
-		if(e.keyCode==35)
-		{
+		if(e.keyCode==35) {
 			$("#pid1").text("End");
 		}
 
-		if(e.keyCode==36)
-		{
+		if(e.keyCode==36) {
 			$("#pid1").text("Home");
 		}
 
-		if(e.keyCode==37)
-		{
+		if(e.keyCode==37) {
 			$("#pid1").text("Left Arrow");
 		}
 		
-		if(e.keyCode==38)
-		{
+		if(e.keyCode==38) {
 			$("#pid1").text("Up Arrow");
 		}
 		
-		if(e.keyCode==39)
-		{
+		if(e.keyCode==39) {
 			$("#pid1").text("Right Arrow");
 		}
 		
-		if(e.keyCode==40)
-		{
+		if(e.keyCode==40) {
 			$("#pid1").text("Down Arrow");
 		}
 		
-		if(e.keyCode==45)
-		{
+		if(e.keyCode==45) {
 			$("#pid1").text("Insert");
 		}
 		
-		if(e.keyCode==46)
-		{
+		if(e.keyCode==46) {
 			$("#pid1").text("Delete");
 		}
 		
-		if(e.keyCode==112)
-		{
+		if(e.keyCode==112) {
 			$("#pid1").text("F1");
 		}
 		
-		if(e.keyCode==113)
-		{
+		if(e.keyCode==113) {
 			$("#pid1").text("F2");
 		}
 
-		if(e.keyCode==114)
-		{
+		if(e.keyCode==114) {
 			$("#pid1").text("F3");
 		}
 		
-		if(e.keyCode==115)
-		{
+		if(e.keyCode==115) {
 			$("#pid1").text("F4");
 		}
 		
-		if(e.keyCode==116)
-		{
+		if(e.keyCode==116) {
 			$("#pid1").text("F5");
 		}
 		
-		if(e.keyCode==117)
-		{
+		if(e.keyCode==117) {
 			$("#pid1").text("F6");
 		}
 		
-		if(e.keyCode==118)
-		{
+		if(e.keyCode==118) {
 			$("#pid1").text("F7");
 		}
 		
-		if(e.keyCode==119)
-		{
+		if(e.keyCode==119) {
 			$("#pid1").text("F8");
 		}
 		
-		if(e.keyCode==120)
-		{
+		if(e.keyCode==120) {
 			$("#pid1").text("F9");
 		}
 		
-		if(e.keyCode==121)
-		{
+		if(e.keyCode==121) {
 			$("#pid1").text("F10");
 		}
 		
-		if(e.keyCode==122)
-		{
+		if(e.keyCode==122) {
 			$("#pid1").text("F11");
 		}
 		
-		if(e.keyCode==123)
-		{
+		if(e.keyCode==123) {
 			$("#pid1").text("F12");
 		}
 		
-		if(e.keyCode==144)
-		{
+		if(e.keyCode==144) {
 			$("#pid1").text("Num Lock");
 		}
 		
-		if(e.keyCode==145)
-		{
+		if(e.keyCode==145) {
 			$("#pid1").text("Scroll Lock");
 		}
 		
-		if(e.keyCode==186)
-		{
+		if(e.keyCode==186) {
 			$("#pid1").text("Semi-colon");
 		}
 		
-		if(e.keyCode==187)
-		{
+		if(e.keyCode==187) {
 			$("#pid1").text("Equal sign");
 		}
 		
-		if(e.keyCode==188)
-		{
+		if(e.keyCode==188) {
 			$("#pid1").text("Comma");
 		}
 		
-		if(e.keyCode==189)
-		{
+		if(e.keyCode==189) {
 			$("#pid1").text("Dash");
 		}
 		
-		if(e.keyCode==190)
-		{
+		if(e.keyCode==190) {
 			$("#pid1").text("Period");
 		}
 		
-		if(e.keyCode==191)
-		{
+		if(e.keyCode==191) {
 			$("#pid1").text("Forward Slash");
 		}
 		
-		if(e.keyCode==192)
-		{
+		if(e.keyCode==192) {
 			$("#pid1").text("Grave Accent");
 		}
 		
-		if(e.keyCode==219)
-		{
+		if(e.keyCode==219) {
 			$("#pid1").text("Open Bracket");
 		}
 		
-		if(e.keyCode==220)
-		{
+		if(e.keyCode==220) {
 			$("#pid1").text("Back Slash");
 		}
 		
-		if(e.keyCode==221)
-		{
+		if(e.keyCode==221) {
 			$("#pid1").text("Close Bracket");
 		}
 		
-		if(e.keyCode==222)
-		{
+		if(e.keyCode==222) {
 			$("#pid1").text("Single Quote");
 		}
 	});
